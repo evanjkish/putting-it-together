5eccee5cfed17fe6b5ddad027663a3c073d685e6
evanjkish <evanjkish@icloud.com>
2021-10-28 00:33:01 -0600

    commit1Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PersonCard from "./components/PersonCard.jsx";
 
 function App() {
+  var persons = [
+    { firstName: "Jane", lastName: "Doe", age: 45, hairColor: "Black" },
+    { firstName: "John", lastName: "Smith", age: 88, hairColor: "Brown" },
+    { firstName: "Millard", lastName: "Fillmore", age: 50, hairColor: "Brown" },
+    { firstName: "Maria", lastName: "Smith", age: 62, hairColor: "Brown" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {persons.map((person) => {
+        return (
+          <PersonCard
+            firstName={person.firstName}
+            lastName={person.lastName}
+            age={person.age}
+            hairColor={person.hairColor}
+          />
+        );
+      })}
     </div>
   );
 }
